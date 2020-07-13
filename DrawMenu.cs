1b92aed257aba1296f4f05f241ebeae44674ad96
Acherons_ <kamron.cole13@gmail.com>
2020-07-13 17:10:23 -0400

    Update DrawMenu.cs

--- DrawMenu.cs
+++ DrawMenu.cs
@@ -17,7 +17,7 @@ namespace UmbraRoR
             {
                 GUI.Button(btn.BtnRect(2, false, "main"), "<color=yellow>Buttons will be availble in game.</color>", buttonStyle);
                 GUI.Button(btn.BtnRect(3, false, "main"), "<color=#11ccee>Created By Neonix#1337.\n Feel Free to Message me on discord.</color>", buttonStyle);
-                GUI.Button(btn.BtnRect(4, false, "main"), "<color=#11ccee>Download the latest version on my github.\nAcher0ns/Umbra-Mod-Menu</color>", buttonStyle);
+                GUI.Button(btn.BtnRect(4, false, "main"), "<color=#11ccee>Download the latest version from unknowncheats.me</color>", buttonStyle);
             }
             else if (Updates.upToDate || Updates.devBuild)
             {
